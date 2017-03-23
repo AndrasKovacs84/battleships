218950f7211a73a10158b56c0afb1b487a6db75e
Laszlo Laun <laun.laszlo@gmail.com>
2017-03-23 11:58:25 +0100

    random ship factory by Laun

--- rand_ship_placement.py
+++ rand_ship_placement.py
@@ -6,11 +6,10 @@ import itertools
 # each other, horizontally, vertically or diagonally.
 
 
-def rand_ship_factory():
+def rand_ship_factory(length):
     global invalid_coords
     invalid_coords = []
-    AI_ships = []
-    AI_ship3 = []
+    AI_ship = []
 
     all_coords = list(range(0, 10))  # contains possible values for X and Y coordinates
     coords1 = []
@@ -18,13 +17,13 @@ def rand_ship_factory():
 
     # starts to generate the 3 long ship
 
-    slice_start = random.randrange(0, len(all_coords) - 2)  # set the start of slicing
-    slice_end = slice_start + 3                             # set the end of slicing
+    slice_start = random.randrange(0, len(all_coords) - (length - 1))  # set the start of slicing
+    slice_end = slice_start + length                             # set the end of slicing
     coords1.append(all_coords[slice_start:slice_end])       # slices a 3 long piece of list from "all_coords"
 
     # generate a random sequence with 3 neighbouring numbers between 0 and 10
     rand_num = random.randrange(0, 10)
-    for i in range(3):
+    for i in range(length):
         coords2.append(rand_num)
 
     print(slice_start)
@@ -33,24 +32,23 @@ def rand_ship_factory():
     print(coords2)
 
     # randomly decides whether coords1 or coords2 represents the horizontal coordinates on the gameboard
-    # "AI_ship3" will be filled up with coordinates
+    # "AI_ship" will be filled up with coordinates
     if random.choice((True, False)):
-        for i in range(3):
-            AI_ship3.append([coords1[0][i], coords2[i]])
+        for i in range(length):
+            AI_ship.append([coords1[0][i], coords2[i]])
     else:
-        for i in range(3):
-            AI_ship3.append([coords2[i], coords1[0][i]])
+        for i in range(length):
+            AI_ship.append([coords2[i], coords1[0][i]])
 
-    print(AI_ship3)             # "AI_ship3 is ready and joins to the AI_ship3 list"
-    AI_ships.append(AI_ship3)   #
-    print(AI_ships)
-    # return AI_ships
+    print(AI_ship)             # "AI_ship is ready and joins to the AI_ship list"
 
-    for i in AI_ship3:
+    # return AI_ship
+
+    for i in AI_ship:
         invalid_coords.append(i)
     print(invalid_coords)
 
-    for i in AI_ship3:
+    for i in AI_ship:
         invalid_coords.append([(i[0] - 1), i[1]])
         invalid_coords.append([(i[0] + 1), i[1]])
         invalid_coords.append([i[0], (i[1] - 1)])
@@ -60,9 +58,9 @@ def rand_ship_factory():
         invalid_coords.append([(i[0] - 1), (i[1] + 1)])
         invalid_coords.append([(i[0] + 1), (i[1] - 1)])
 
-        # invalid_coords.append([(AI_ship3[0][0] - 1), AI_ship3[0][1]])
-
-    invalid_coords = list(set(invalid_coords))
+        # invalid_coords.append([(AI_ship[0][0] - 1), AI_ship[0][1]])
+    invalid_coords.sort()
+    invalid_coords = list(invalid_coords for invalid_coords, _ in itertools.groupby(invalid_coords))
     print(invalid_coords)
 
-rand_ship_factory()
+rand_ship_factory(5)
