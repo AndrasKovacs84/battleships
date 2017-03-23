7fc862472b585f12e42a2afc82dbc04dd6a78f76
Laszlo Laun <laun.laszlo@gmail.com>
2017-03-23 09:00:59 +0100

    random ship factory by Laun

---
+++ rand_ship_placement.py
@@ -0,0 +1,46 @@
+import random
+
+# Generates a 3, 4, and 5 square long ships with random position.
+# The generated ships cannot overlap and cannot occupy squares next to
+# each other, horizontally, vertically or diagonally.
+
+
+def rand_ship_factory():
+    AI_ships = []
+    AI_ship3 = []
+
+    all_coords = list(range(0, 10))  # contains possible values for X and Y coordinates
+    coords1 = []
+    coords2 = []
+
+    # starts to generate the 3 long ship
+
+    slice_start = random.randrange(0, len(all_coords) - 2)  # set the start of slicing
+    slice_end = slice_start + 3                             # set the end of slicing
+    coords1.append(all_coords[slice_start:slice_end])       # slices a 3 long piece of list from "all_coords"
+
+    # generate a random sequence with 3 neighbouring numbers between 0 and 10
+    rand_num = random.randrange(0, 10)
+    for i in range(3):
+        coords2.append(rand_num)
+
+    print(slice_start)
+    print(slice_end)
+    print(coords1)
+    print(coords2)
+
+    # randomly decides whether coords1 or coords2 represents the horizontal coordinates on the gameboard
+    # "AI_ship3" will be filled up with coordinates
+    if random.choice((True, False)):
+        for i in range(3):
+            AI_ship3.append([coords1[0][i], coords2[i]])
+    else:
+        for i in range(3):
+            AI_ship3.append([coords2[i], coords1[0][i]])
+
+    print(AI_ship3)             # "AI_ship3 is ready and joins to the AI_ship3 list"
+    AI_ships.append(AI_ship3)   #
+    print(AI_ships)
+    # return AI_ships
+
+rand_ship_factory()
